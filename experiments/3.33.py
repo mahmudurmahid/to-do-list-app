59472e2041e9038877c32fb5e9626a863d879ae7
mahmudurmahid <m.mahid@icloud.com>
2025-03-16 23:54:47 +0000

    section 3 complete

--- experiments/3.33.py
+++ experiments/3.33.py
@@ -8,10 +8,13 @@ while True:
         case "add":
             todo = input("Enter your todo: ")
             todos.append(todo)
-        case "show":
+        case "show" | "display": # or operator
             for item in todos:
+                item = item.title()
                 print(item)
         case "exit":
             break
+        case _: # default case
+            print("Invalid command")
 
 print("Goodbye!")
---
+++ experiments/3.34.py
@@ -0,0 +1,6 @@
+meals = ["pasta", "pizza", "salad", "soup", "steak"]
+
+for meal in meals:
+    print(meal.capitalize())
+
+print("Goodbye!")
