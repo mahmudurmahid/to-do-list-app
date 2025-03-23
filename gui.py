97989220b40cf8e6da82368b00b58770237fa9ea
mahmudurmahid <m.mahid@icloud.com>
2025-03-23 21:02:10 +0000

    up to section 18.171, added complete and exit buttons

--- gui.py
+++ gui.py
@@ -5,12 +5,16 @@ label = sg.Text("Type in a to do:")
 input_box = sg.InputText(tooltip="Enter to do", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=get_todos(), key="todos", enable_events=True, size=[45, 10])
+
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To Do App",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -35,10 +39,21 @@ while True:
             todos[index] = new_todo
             write_todos(todos)
             window["todos"].update(values=todos)
+        
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = get_todos()
+            todos.remove(todo_to_complete)
+            write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        
+        case "Exit":
+            break
 
         case "Todos":
             window["todo"].update(value=values["todos"][0])
-            
+
         case sg.WIN_CLOSED:
             break
 
