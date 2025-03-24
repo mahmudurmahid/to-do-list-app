7b691e72c237e8f9071f27c50b03a417267e8029
mahmudurmahid <m.mahid@icloud.com>
2025-03-24 15:59:01 +0000

    up to section 17.184

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ st.title("My To Do App")
 st.subheader("Tasks for you to complete")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Enter a to do:", placeholder="Add a new to do item here",
               on_change=add_todo, key="new_todo")
