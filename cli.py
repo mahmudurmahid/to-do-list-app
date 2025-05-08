84a75d028a63950815e5c0bedfa57bec386918bb
mahmudurmahid <m.mahid@icloud.com>
2025-05-08 15:41:00 +0100

    refactored cli.py code without functional change

--- cli.py
+++ cli.py
@@ -8,14 +8,10 @@ while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip().lower()
 
-
     if user_action.startswith("add"):
         todo = user_action[4:]
-
         todos = get_todos()
-
         todos.append(todo + "\n")
-
         write_todos(todos)
 
     elif user_action.startswith("show"):
@@ -30,12 +26,9 @@ while True:
         try:
             number = int(user_action[5:])
             number = number - 1
-
             todos = get_todos()
-            
             new_todo = input("Enter the new todo: ")
             todos[number] = new_todo + "\n"
-
             write_todos(todos)
         except ValueError:
             print("Invalid number. Please try again.")
@@ -44,15 +37,11 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-
             todos = get_todos()
-
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
-
             write_todos(todos)
-
             message = f"Todo number {todo_to_remove} has been completed and removed from the list."
             print(message)
         except IndexError:
